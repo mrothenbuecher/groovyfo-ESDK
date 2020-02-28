39e96d248b55a25e3dc6ba739550012054a8f9e8
mrothenbuecher <ich@m-rothenbuecher.de>
2020-02-28 18:18:14 +0100

    Test implementiert
    TODO FOP ow1/GROOVYFO.UNITTEST ggf. erzeugen im Testsystem

--- src/integTest/java/de/finetech/groovy/GroovyFOTest.java
+++ src/integTest/java/de/finetech/groovy/GroovyFOTest.java
@@ -1,23 +1,27 @@
 package de.finetech.groovy;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import de.abas.erp.db.infosystem.standard.is.RunFOP;
-import de.abas.esdk.test.util.DoNotFailOnError;
 import de.abas.esdk.test.util.EsdkIntegTest;
 
 public class GroovyFOTest extends EsdkIntegTest {
 	
 	@Test
 	public void executeTest() {
-	    //int value = EKS.eingabe("java:GroovyFO@grvfo ow1/GROOVYFO.UNITTEST");
-	    //assertEquals("Unittest gescheitert", value, 0);
 	    RunFOP runFOP = ctx.openInfosystem(RunFOP.class);
 		runFOP.setKommando("java:GroovyFO@grvfo ow1/GROOVYFO.UNITTEST");
 		runFOP.invokeStart();
 
-		//System.out.println("Messges: " + getMessages());
-		//System.out.println("Errors : " + getErrors());
+		for(String msg: this.getErrors()) {
+			System.out.println(msg);
+		}
+		for(String error: this.getErrors()) {
+			System.err.println(error);
+		}
+		assertEquals("Es gab Fehler", this.getErrors().size(), 0);
 	}
 	
 }
