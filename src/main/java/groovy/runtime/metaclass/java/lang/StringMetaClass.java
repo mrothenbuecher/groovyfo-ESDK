77da3da4b511f845fa36638898b001579a1aeab7
mrothenbuecher <ich@m-rothenbuecher.de>
2020-08-31 13:39:10 +0200

    Updated String funktionen

--- src/main/java/groovy/runtime/metaclass/java/lang/StringMetaClass.java
+++ src/main/java/groovy/runtime/metaclass/java/lang/StringMetaClass.java
@@ -13,11 +13,34 @@ public class StringMetaClass extends DelegatingMetaClass {
 		super(metaClass);
 	}
 
+	/**
+	 * 
+	 * see https://extranet.abas.de/sub_de/help/hd/html/9.1.5.14.html#FO.OPER.STR.AZ
+	 * 
+	 */
 	@Override
 	public Object invokeMethod(Object object, String name, Object[] args) {
+		// implementiert +"test"
+		if (name.equals("positive")) {
+			return object.toString().length();
+		}
+		// implementiert -"test"
+		if (name.equals("negative")) {
+			return object.toString().toUpperCase().replaceAll("\u00C4", "AE").replaceAll("\u00DC", "UE")
+					.replaceAll("\u00D6", "OE").replaceAll("\u00DF", "SS");
+		}
+
+		// implementiert "test"/"test"
+		// Achtung implementiert keinen Matchcode sondern regex
+		if (name.equals("div")) {
+			if (args != null && args.length == 1 && args[0] instanceof String) {
+				return object.toString().matches(args[0].toString());
+			}
+		}
+
 		// implementiert "test" >> 3
 		if (name.equals("rightShift")) {
-			if (args.length == 1) {
+			if (args != null && args.length == 1) {
 				if (args[0] instanceof Integer) {
 					String str = object.toString();
 					int x = ((Integer) args[0]).intValue();
@@ -25,33 +48,27 @@ public class StringMetaClass extends DelegatingMetaClass {
 						return str.substring(str.length() - x);
 					}
 					return str;
-				} else {
-					throw new IllegalArgumentException("wrong argument type, should be integer");
+				} else if (args[0] instanceof String && args[0].toString().isEmpty()) {
+					return object.toString().replaceAll("\\s+$", "");
 				}
-			} else {
-				throw new IllegalArgumentException("too many arguments");
 			}
 		}
 		// implementiert "test" << 3
 		if (name.equals("leftShift")) {
-			if (args.length == 1) {
+			if (args != null && args.length == 1) {
 				if (args[0] instanceof Integer) {
 					String str = object.toString();
 					int x = ((Integer) args[0]).intValue();
 					if (str.length() > x) {
-						return str.substring(0,x);
+						return str.substring(0, x);
 					}
 					return str;
-				} else {
-					throw new IllegalArgumentException("wrong argument type, should be integer");
+				} else if (args[0] instanceof String && args[0].toString().isEmpty()) {
+					return object.toString().replaceAll("^\\s+", "");
 				}
-			} else {
-				throw new IllegalArgumentException("too many arguments");
 			}
 		}
-		else {
-			return super.invokeMethod(object, name, args);
-		}
+		return super.invokeMethod(object, name, args);
 	}
 
 }
---
+++ src/main/java/groovy/runtime/metaclass/java/lang/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ * 
+ * see https://groovy-lang.org/metaprogramming.html#_magic_package
+ *
+ * @author M. Rothenbücher
+ */
+package groovy.runtime.metaclass.java.lang;
