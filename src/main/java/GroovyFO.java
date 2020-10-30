66c4f5f1b39173afe2ff582d8c7f44165cb29296
mrothenbuecher <ich@m-rothenbuecher.de>
2020-10-30 12:17:00 +0100

    Update GroovyFO.java

--- src/main/java/GroovyFO.java
+++ src/main/java/GroovyFO.java
@@ -1,5 +1,11 @@
+import java.util.Enumeration;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import de.abas.eks.jfop.FOPException;
 import de.abas.eks.jfop.remote.ContextRunnable;
+import de.abas.eks.jfop.remote.FO;
 import de.abas.eks.jfop.remote.FOPSessionContext;
 import de.finetech.groovy.ScriptExecutor;
 
@@ -7,6 +13,13 @@ public class GroovyFO implements ContextRunnable {
 
 	@Override
 	public int runFop(FOPSessionContext arg0, String[] arg1) throws FOPException {
+		Enumeration<Logger> e = LogManager.getCurrentLoggers();
+		FO.println("Beginne Test");
+		while(e.hasMoreElements()) {
+			Logger l = e.nextElement();
+			FO.println(l.getName());
+		}
+		FO.println("Beende Test");
 		return ScriptExecutor.executeScript(arg0, arg1);
 	}
 
