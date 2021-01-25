bedd5e290d533ddca449f3bac4acc353ad9a15e9
Michael Rothenbücher <ich@m-rothenbuecher.de>
2021-01-25 14:20:04 +0100

    Update TypGuesser.java

--- src/main/java/de/finetech/groovy/utils/datatypes/TypGuesser.java
+++ src/main/java/de/finetech/groovy/utils/datatypes/TypGuesser.java
@@ -2,6 +2,9 @@ package de.finetech.groovy.utils.datatypes;
 
 import java.util.regex.Pattern;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import de.abas.ceks.jedp.EDPEKSArtInfo;
 
 /**
@@ -11,13 +14,15 @@ import de.abas.ceks.jedp.EDPEKSArtInfo;
  */
 public class TypGuesser {
 
+	private static final Logger log = LoggerFactory.getLogger(TypGuesser.class);
+	
 	public static enum PossibleDatatypes {
 		INTEGER, DOUBLE, DOUBLED, DOUBLET, DOUBLEDT, BOOLEAN, ABASDATE, ABASPOINTER, STRING
 	}
 
 	// regulüre Ausdrücke zum erkennen von Variablen arten
-	private static Pattern integerPattern = Pattern.compile("(?:I(?:[0-9]*|P[^ ]*|N[^ ]*)|(?:K[^ ]*)|(?:AN[^ ]*))");
-	private static Pattern doublePattern = Pattern.compile("(?:(?:R[^ ]*)|(?:M[^ ]*))");
+	//private static Pattern integerPattern = Pattern.compile("(?:I(?:[0-9]*|P[^ ]*|N[^ ]*)|(?:K[^ ]*)|(?:AN[^ ]*))");
+	//private static Pattern doublePattern = Pattern.compile("(?:(?:R[^ ]*)|(?:M[^ ]*))");
 	private static Pattern doubledPattern = Pattern.compile("(?:(?:R[^ ]*D[^ ]*)|(?:M[^ ]*D[^ ]*))");
 	private static Pattern doubletPattern = Pattern.compile("(?:(?:R[^ ]*T[^ ]*)|(?:M[^ ]*T[^ ]*))");
 	private static Pattern doubledtPattern = Pattern.compile("(?:(?:R[^ ]*DT[^ ]*)|(?:M[^ ]*DT[^ ]*))");
@@ -36,77 +41,65 @@ public class TypGuesser {
 	public static PossibleDatatypes getClassOfType(String abasType) {
 		EDPEKSArtInfo nfo = new EDPEKSArtInfo(abasType);
 		
-		/*
-		if (integerPattern.matcher(abasType).matches()) {
-			return PossibleDatatypes.INTEGER;
-		} 
-		*/
 		if(nfo.getIntegerDigits() > 0 && nfo.getFractionDigits() == 0) {
+			if(log.isTraceEnabled()) {
+				log.trace(abasType+" => integer");
+			}
 			return PossibleDatatypes.INTEGER;
 		}
 		
-		
 		// real tausender und dezimal
-		/*
-		if (doublePattern.matcher(abasType).matches()) {
-			// Real
-			if (doubledtPattern.matcher(abasType).matches()) {
-				return PossibleDatatypes.DOUBLEDT;
-			} // real tausender
-			if (doubletPattern.matcher(abasType).matches()) {
-				return PossibleDatatypes.DOUBLET;
-			} // real dezimal trennzeichen
-			if (doubledPattern.matcher(abasType).matches()) {
-				return PossibleDatatypes.DOUBLED;
-			} 
-			return PossibleDatatypes.DOUBLE;
-		}*/
 		if (nfo.getIntegerDigits() > 0 && nfo.getFractionDigits() > 0) {
 			// Real
 			if (doubledtPattern.matcher(abasType).matches()) {
+				if(log.isTraceEnabled()) {
+					log.trace(abasType+" => doubleDT");
+				}
 				return PossibleDatatypes.DOUBLEDT;
 			} // real tausender
 			if (doubletPattern.matcher(abasType).matches()) {
+				if(log.isTraceEnabled()) {
+					log.trace(abasType+" => doubleT");
+				}
 				return PossibleDatatypes.DOUBLET;
 			} // real dezimal trennzeichen
 			if (doubledPattern.matcher(abasType).matches()) {
+				if(log.isTraceEnabled()) {
+					log.trace(abasType+" => doubleD");
+				}
 				return PossibleDatatypes.DOUBLED;
-			} 
+			}
+			if(log.isTraceEnabled()) {
+				log.trace(abasType+" => double");
+			}
 			return PossibleDatatypes.DOUBLE;
 		} 
 		
 		
 		// bool
 		if (boolPattern.matcher(abasType).matches()) {
+			if(log.isTraceEnabled()) {
+				log.trace(abasType+" => boolean");
+			}
 			return PossibleDatatypes.BOOLEAN;
 		}
 		if (AbasDate.isDate(abasType)) {
+			if(log.isTraceEnabled()) {
+				log.trace(abasType+" => abasdate");
+			}
 			return PossibleDatatypes.ABASDATE;
 		} // Zeiger also
 		if (pointerPattern.matcher(abasType).matches()) {
+			if(log.isTraceEnabled()) {
+				log.trace(abasType+" => abaspointer");
+			}
 			return PossibleDatatypes.ABASPOINTER;
 		}
 		// Strings
+		if(log.isTraceEnabled()) {
+			log.trace(abasType+" => string");
+		}
 		return PossibleDatatypes.STRING;
-		/*
-		 * Matcher matcher = newPattern.matcher(abasType); if (matcher.find()) { if
-		 * (matcher.group(1) != null) { return PossibleDatatypes.INTEGER; } if
-		 * (matcher.group(2) != null) { // System.out.println(
-		 * "2 PossibleDatatypes.DOUBLE -> "+ matcher.group(2)); // real tausender und
-		 * dezimal if (doubledtPattern.matcher(abasType).matches()) { return
-		 * PossibleDatatypes.DOUBLEDT; } // real tausender if
-		 * (doubletPattern.matcher(abasType).matches()) { return
-		 * PossibleDatatypes.DOUBLET; } // real dezimal trennzeichen if
-		 * (doubledPattern.matcher(abasType).matches()) { return
-		 * PossibleDatatypes.DOUBLED; } // Real if
-		 * (doublePattern.matcher(abasType).matches()) { return
-		 * PossibleDatatypes.DOUBLE; } } if (matcher.group(3) != null) { //
-		 * System.out.println( "2 PossibleDatatypes.BOOL -> "+ matcher.group(3)); return
-		 * PossibleDatatypes.BOOLEAN; } if (matcher.group(4) != null) { //
-		 * System.out.println( "2 PossibleDatatypes.ABASPOINTER -> "+ matcher.group(4));
-		 * return PossibleDatatypes.ABASPOINTER; } } if (AbasDate.isDate(abasType)) {
-		 * return PossibleDatatypes.ABASDATE; } return PossibleDatatypes.STRING;
-		 */
 	}
 
 }
